d781d44676899c1e6459a5f1559a0dcdb30ef77a
Elias Schramm <privat@eps-dev.de>
2022-08-13 16:36:55 +0200

    feat(fe): Open API generation from swagger.json

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
+import { ApiModule } from './api/api.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +14,8 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
     imports: [
         BrowserModule,
         AppRoutingModule,
+        HttpClientModule,
+        ApiModule,
         BrowserAnimationsModule,
         ButtonModule,
     ],
