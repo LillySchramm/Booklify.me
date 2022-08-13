5271c0a63a5f831b5d506121587c903e0b4cb54f
Elias Schramm <privat@eps-dev.de>
2022-08-13 17:50:38 +0200

    feat(be): Add CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,11 @@ import bodyParser from 'body-parser';
 import { RegisterRoutes } from '../build/routes';
 import express, { Response as ExResponse, Request as ExRequest } from 'express';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 
 export const app = express();
 
+app.use(cors());
 // Use body parser to read sent json payloads
 app.use(
     bodyParser.urlencoded({
