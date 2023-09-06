07fa9cfb9d31997b853dcfb8942f92f2d678787e
Lilly Schramm <privat@eps-dev.de>
2023-09-06 16:34:38 +0200

    feat: Make Logo Clickable

--- frontend/src/app/common/header/header.component.ts
+++ frontend/src/app/common/header/header.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [CommonModule, MatToolbarModule, MatButtonModule, TranslocoModule],
+    imports: [CommonModule, MatToolbarModule, MatButtonModule, TranslocoModule, RouterModule],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
 })
