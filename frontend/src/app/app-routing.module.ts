fc3d6d0af9532f601a3d6bc244385de37101f448
Lilly Schramm <privat@eps-dev.de>
2023-09-07 15:37:42 +0200

    feat: Lazy Load Pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-
 const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./pages/login/login.component').then(
+                (m) => m.LoginComponent,
+            ),
+    },
 ];
 
 @NgModule({
