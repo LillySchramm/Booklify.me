f5eca56eaeb4fb0bfecbd895de155cd65e66beb3
Lilly Schramm <privat@eps-dev.de>
2023-09-09 18:52:08 +0200

    feat: Make Access Token Expiration Configurable

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,6 +5,9 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { SecretsModule } from 'src/secrets/secrets.module';
+import * as config from 'config';
+
+const TOKEN_EXPIRATION = config.get<string>('security.access_token_expiration');
 
 @Module({
     imports: [
@@ -12,8 +15,7 @@ import { SecretsModule } from 'src/secrets/secrets.module';
         JwtModule.register({
             global: true,
             signOptions: {
-                expiresIn: '24h',
-                algorithm: 'RS256',
+                expiresIn: TOKEN_EXPIRATION,
                 issuer: 'Mangalist',
             },
         }),
